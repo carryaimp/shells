ecf7e0cd6ebcb57215e3027708a8e0f458f396bd
root <jia2jiayuan@163.com>
2018-05-06 11:49:26 +0800

    重构个性化persion_vim脚本一键配置

--- personal_vim.sh
+++ personal_vim.sh
@@ -7,27 +7,65 @@
 # 4 space == 1 tab, write or read files will display number of lines, vi = vim
 
 . /etc/init.d/functions
-# 1 copy, 2 write setting, 3 source 
 
 one_shif=4
 _tabstop=4
+vimConf="/etc/vimrc"
+persionVimConf="$HOME/.vimrc"
+templatePath="$HOME/.vim/template"
+templateName="temp.sh"
 
-personal_vim() {
-  [ -f /etc/vimrc ] && {
-    cd $HOME
-    /bin/cp /etc/vimrc .
-    /bin/mv vimrc .vimrc
-    echo -e "set nu\nset smartindent\nset tabstop=${_tabstop}\nset shiftwidth=${one_shif}\nset expandtab\nautocmd BufNewFile *.sh 0r /root/.vim/template/tmp.sh" >> .vimrc
-    sed -i "5i alias vi='vim'" .bashrc
-    [ -d .vim/template ] || {
-        mkdir .vim/template -p
-    }
-    echo "#!/bin/sh" >> .vim/template/tmp.sh
-    echo "# author: beimenchuixue" >> .vim/template/tmp.sh
-    echo "# email: 422083556@qq.com" >> .vim/template/tmp.sh
-    echo "# blog: http://www.cnblogs.com/2bjiujiu/" >> .vim/template/tmp.sh
-    action "$HOME person vim setting is " /bin/true  
+function sureOk {
+    [ $1 -eq 0 ] && {
+        action "$2 is"  /bin/true
+    } || {
+        action "$2 is"  /bin/false
     }
 }
 
-personal_vim
+function setVimConf {
+    [ -f $persionVimConf ] || {
+        cp $vimConf $persionVimConf
+    }
+    cat >>$persionVimConf<<EOF
+set nu
+set smartindent
+set tabstop=${_tabstop}
+set shiftwidth=${one_shif}
+set expandtab
+autocmd BufNewFile *.sh 0r $templatePath/$templateName
+EOF
+    sureOk $? "setVimConf"
+    sed -i "5i alias vi='vim'" /$HOME/.bashrc
+    sureOk $? "vi alias vim"
+}
+
+function setVimTemplate {
+    [ -d $templatePath ] || {
+        mkdir -p $templatePath
+        sureOk $? "init templatePath"
+    }
+    cat >>$templatePath/$templateName<<EOF
+#!/bin/sh
+# author: beimenchuixue
+# email: 422083556@qq.com
+# blog: http://www.cnblogs.com/2bjiujiu/
+
+. /etc/init.d/functions
+
+function sureOk {
+    [ \$1 -eq 0 ] && {
+        action "\$2 is"  /bin/true
+    } || {
+        action "\$2 is"  /bin/false
+    }
+}
+EOF
+    sureOk $? "setVimTemplate"
+}
+
+function main_BeiMenChuiXue {
+    setVimConf
+    setVimTemplate
+}
+main_BeiMenChuiXue
